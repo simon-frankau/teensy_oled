4df5538934ab633f8ec66ea439ce5fc38a5a857e
Simon Frankau <sgf@arbitrary.name>
2021-01-31 21:56:08 +0000

    Add minimal LCD start-up code

--- teensy_lcd.c
+++ teensy_lcd.c
@@ -31,6 +31,101 @@
 #define CPU_125kHz      0x07
 #define CPU_62kHz       0x08
 
+// I2C on D0/D1: SCL on D0, SDA on D1
+//
+// In I2C the lines float high and are actively pulled low, so we set
+// them to output zero, and enable/disable driving it low.
+#define SCL_CONFIG (DDRD &= ~(1 << 0), PORTD &= ~(1 << 0))
+#define SDA_CONFIG (DDRD &= ~(1 << 1), PORTD &= ~(1 << 1))
+
+#define SCL_HIGH (DDRD &= ~(1 << 0))
+#define SCL_LOW  (DDRD |= (1 << 0))
+#define SCL_READ (PIND & (1 << 0))
+
+#define SDA_HIGH (DDRD &= ~(1 << 1))
+#define SDA_LOW  (DDRD |= (1 << 1))
+#define SDA_READ (PIND & (1 << 1))
+
+#define I2C_READ 1
+#define I2C_WRITE 0
+
+static void i2c_init(void)
+{
+    SCL_CONFIG;
+    SDA_CONFIG;
+}
+
+// TODO: Currently works because I've set the clock slow. We may need delays.
+static void i2c_send_bit(int i)
+{
+    // Set data up first.
+    if (i) {
+        SDA_HIGH;
+    } else {
+        SDA_LOW;
+    }
+
+    // Then cycle the clock.
+    SCL_HIGH;
+    // Receiver may be holding clock down to clock stretch...
+    while (SCL_READ == 0) {
+    }
+    SCL_LOW;
+}
+
+static void i2c_send_byte(char c)
+{
+    i2c_send_bit(c & 0x80);
+    i2c_send_bit(c & 0x40);
+    i2c_send_bit(c & 0x20);
+    i2c_send_bit(c & 0x10);
+    i2c_send_bit(c & 0x08);
+    i2c_send_bit(c & 0x04);
+    i2c_send_bit(c & 0x02);
+    i2c_send_bit(c & 0x01);
+
+    // Ack bit is sent by device. Don't drive SDA during this.
+    SDA_HIGH;
+    int acked = !SDA_READ;
+    // And ack the ack/nack.
+    SCL_HIGH;
+    // Receiver may be holding clock down to clock stretch...
+    while (SCL_READ == 0) {
+    }
+    SCL_LOW;
+
+    if (acked) {
+        print("ACK\n");
+    } else {
+        print ("NACK\n");
+    }
+}
+
+static void lcd_init(void)
+{
+    // Initiate request with a start condition.
+    SDA_LOW;
+    SCL_LOW;
+
+    // Start I2C request with the display address.
+    // TODO: Could be the other addr?
+    i2c_send_byte(0x78);
+
+    // Minimal start-up sequence:
+
+    // Turn on the charge pump, as the breakout board doesn't have a
+    // higher-voltage supply.
+    i2c_send_byte(0x00); i2c_send_byte(0x8d); i2c_send_byte(0x14);
+    // Turn display on.
+    i2c_send_byte(0x00); i2c_send_byte(0xAF);
+    // Turn on all pixels.
+    i2c_send_byte(0x00); i2c_send_byte(0xA5);
+
+    // Stop condition.
+    SCL_HIGH;
+    SDA_HIGH;
+}
+
 int main(void)
 {
     // CPU prescale must be set with interrupts disabled. They're off
@@ -40,6 +135,7 @@ int main(void)
     CPU_PRESCALE(CPU_250kHz);
     LED_CONFIG;
     LED_OFF;
+    i2c_init();
 
     // Initialise USB for debug, but don't wait.
     usb_init();
@@ -51,5 +147,6 @@ int main(void)
         LED_OFF;
         _delay_ms(500);
         print("Hello, world\n");
+        lcd_init();
     }
 }
