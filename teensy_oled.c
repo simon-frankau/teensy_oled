9a44e32652460803bf34a1711f118e6cf222ef5d
Simon Frankau <sgf@arbitrary.name>
2021-02-02 20:22:51 +0000

    Start to remove macros. It's not the '80s.

--- teensy_oled.c
+++ teensy_oled.c
@@ -15,28 +15,65 @@
 #include "usb_debug_only.h"
 #include "print.h"
 
+////////////////////////////////////////////////////////////////////////
+// CPU prescaler
+//
 
-#define LED_ON          (PORTD |= (1<<6))
-#define LED_OFF         (PORTD &= ~(1<<6))
-#define LED_CONFIG      (DDRD |= (1<<6))
+static const char CPU_16MHz  = 0x00;
+static const char CPU_8MHz   = 0x01;
+static const char CPU_4MHz   = 0x02;
+static const char CPU_2MHz   = 0x03;
+static const char CPU_1MHz   = 0x04;
+static const char CPU_500kHz = 0x05;
+static const char CPU_250kHz = 0x06;
+static const char CPU_125kHz = 0x07;
+static const char CPU_62kHz  = 0x08;
 
-#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
-#define CPU_16MHz       0x00
-#define CPU_8MHz        0x01
-#define CPU_4MHz        0x02
-#define CPU_2MHz        0x03
-#define CPU_1MHz        0x04
-#define CPU_500kHz      0x05
-#define CPU_250kHz      0x06
-#define CPU_125kHz      0x07
-#define CPU_62kHz       0x08
+static inline void cpu_prescale(char i)
+{
+    CLKPR = 0x80;
+    CLKPR = i;
+}
+
+////////////////////////////////////////////////////////////////////////
+// LED
+//
+
+static inline void led_init(void)
+{
+    DDRD |= 1 << 6;
+}
+
+static inline void led_on(void)
+{
+    PORTD |= 1 << 6;
+}
+
+static inline void led_off(void)
+{
+    PORTD &= ~(1 << 6);
+}
+
+////////////////////////////////////////////////////////////////////////
+// Low-level I2C config
+//
 
 // I2C on D0/D1: SCL on D0, SDA on D1
-//
-// In I2C the lines float high and are actively pulled low, so we set
-// them to output zero, and enable/disable driving it low.
-#define SCL_CONFIG (DDRD &= ~(1 << 0), PORTD &= ~(1 << 0))
-#define SDA_CONFIG (DDRD &= ~(1 << 1), PORTD &= ~(1 << 1))
+static const char SCL_IDX = 0;
+static const char SDA_IDX = 1;
+
+static void i2c_init(void)
+{
+    // In I2C the lines float high and are actively pulled low, so we
+    // set them to output zero, and enable/disable driving it low.
+
+    // SCL
+    DDRD &= ~(1 << SCL_IDX);
+    PORTD &= ~(1 << SCL_IDX);
+    // SDA
+    DDRD &= ~(1 << SDA_IDX);
+    PORTD &= ~(1 << SDA_IDX);
+}
 
 #define SCL_HIGH (DDRD &= ~(1 << 0))
 #define SCL_LOW  (DDRD |= (1 << 0))
@@ -48,47 +85,6 @@
 
 #define I2C_READ 1
 #define I2C_WRITE 0
-
-static const char i2c_init_instrs[] = {
-    // Data sheet recommended initialisation sequence:
-
-    // Set mux
-    0x00, 0xa8, 0x1f, // Only 32 rows
-    // Set display offset
-    0x00, 0xd3, 0x00,
-    // Set display start line
-    0x00, 0x40,
-    // Set segment remap
-    0x00, 0xa0,
-    // Set COM scan direction
-    0x00, 0xc0,
-    // Set COM pin hw conf
-    0x00, 0xda, 0x02, // This display only uses alternate COM lines.
-    // Contrast control
-    0x00, 0x81, 0x7f,
-    // Disable entire display on
-    0x00, 0xa4,
-    // Set normal display
-    0x00, 0xa6,
-    // Set oscillator frequency
-    0x00, 0xd5, 0x80,
-    // Enable charge pump regulator
-    0x00, 0x08d, 0x14,
-    // Turn display on.
-    0x00, 0xaf,
-
-    // Turn on all pixels.
-    0x00, 0xa5,
-};
-
-static const int i2c_init_instrs_len =
-    sizeof(i2c_init_instrs) / sizeof(*i2c_init_instrs);
-
-static void i2c_init(void)
-{
-    SCL_CONFIG;
-    SDA_CONFIG;
-}
 
 // Cycles the clock high then low again. May wait for a receiver
 // holding the clock down for clock stretching.
@@ -134,6 +130,45 @@ static void i2c_send_byte(char c)
     }
 }
 
+////////////////////////////////////////////////////////////////////////
+// OLED
+//
+
+static const char oled_init_instrs[] = {
+    // Data sheet recommended initialisation sequence:
+
+    // Set mux
+    0x00, 0xa8, 0x1f, // Only 32 rows
+    // Set display offset
+    0x00, 0xd3, 0x00,
+    // Set display start line
+    0x00, 0x40,
+    // Set segment remap
+    0x00, 0xa0,
+    // Set COM scan direction
+    0x00, 0xc0,
+    // Set COM pin hw conf
+    0x00, 0xda, 0x02, // This display only uses alternate COM lines.
+    // Contrast control
+    0x00, 0x81, 0x7f,
+    // Disable entire display on
+    0x00, 0xa4,
+    // Set normal display
+    0x00, 0xa6,
+    // Set oscillator frequency
+    0x00, 0xd5, 0x80,
+    // Enable charge pump regulator
+    0x00, 0x08d, 0x14,
+    // Turn display on.
+    0x00, 0xaf,
+
+    // Turn on all pixels.
+    0x00, 0xa5,
+};
+
+static const int oled_init_instrs_len =
+    sizeof(oled_init_instrs) / sizeof(*oled_init_instrs);
+
 static void oled_init(void)
 {
     // Initiate request with a start condition.
@@ -144,8 +179,8 @@ static void oled_init(void)
     // TODO: Could be the other addr?
     i2c_send_byte(0x78);
 
-    for (int i = 0; i < i2c_init_instrs_len; i++) {
-        i2c_send_byte(i2c_init_instrs[i]);
+    for (int i = 0; i < oled_init_instrs_len; i++) {
+        i2c_send_byte(oled_init_instrs[i]);
     }
 
     // Stop condition.
@@ -159,9 +194,9 @@ int main(void)
     // when the CPU starts.
     //
     // Don't forget to sync this with F_CPU in the Makefile.
-    CPU_PRESCALE(CPU_250kHz);
-    LED_CONFIG;
-    LED_OFF;
+    cpu_prescale(CPU_250kHz);
+    led_init();
+    led_off();
     i2c_init();
 
     // Initialise USB for debug, but don't wait.
@@ -169,9 +204,9 @@ int main(void)
 
     // Blink and print. \o/
     while (1) {
-        LED_ON;
+        led_on();
         _delay_ms(500);
-        LED_OFF;
+        led_off();
         _delay_ms(500);
         print("Hello, world\n");
         oled_init();
